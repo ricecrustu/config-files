4a192621f64690f4f0a559afc7cd1e2eccc7d95e
ricecrustu <schtu93@gmail.com>
2025-07-17 04:33:29 -0400

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,14 +1,11 @@
--- This file is automatically loaded by lazyvim.config.init
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
--- DO NOT USE `LazyVim.safe_keymap_set` IN YOUR OWN CONFIG!!
--- use `vim.keymap.set` instead
 local r = require("utils.remaps")
 
 -- better up/down
 r.noremap({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", "Down", { expr = true, silent = true })
--- r.noremap({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", "Down", { expr = true, silent = true })
 r.noremap({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", "Up", { expr = true, silent = true })
--- r.noremap({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", "Up", { expr = true, silent = true })
 
 -- better left/right
 r.noremap({ "n", "v", "o" }, "<S-h>", "^", "Start of Line")
@@ -42,8 +39,14 @@ r.noremap("x", "<leader>rw", [[y:%s/<C-r>"//gcI<Left><Left><Left><Left>]], "Repl
 r.map("n", "yc", "yygccp", "Copy and Comment Out Current Line then paste it below")
 
 
--- save file
+-- File
 r.noremap({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", "Save File")
+r.noremap("n", "<Leader>fP", function() 
+    vim.cmd("normal! ggVGp")
+end, "Paste clipboard to file (replace all)")
+r.noremap("n", "<Leader>fY", function() 
+    vim.cmd("%y")
+end, "Copy file to clipboard")
 
 -- better indenting
 r.noremap("v", "<", "<gv", "Unindent", {silent = true})
